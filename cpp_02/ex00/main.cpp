4b8232c5ec99d38460c87e7eda858f32d50e513b
devup_desktop <boulhoujjatmehdi@gmail.com>
2023-08-02 13:24:41 +0100

    02/02

--- cpp_02/ex00/main.cpp
+++ cpp_02/ex00/main.cpp
@@ -13,3 +13,26 @@ std::cout << a.getRawBits() << std::endl;
 std::cout << c.getRawBits() << std::endl;
 return 0;
 }
+
+// int main( void ) {
+// 	// Fixed::setVerbose(true);
+// 	Fixed a;
+// 	std::cout << a.getRawBits() << std::endl;
+
+// 	a.setRawBits(21);
+// 	std::cout << a.getRawBits() << std::endl;
+
+// 	Fixed b( a );
+// 	std::cout << b.getRawBits() << std::endl;
+
+// 	b.setRawBits(42);
+// 	Fixed c;
+// 	c = b;
+// 	std::cout << b.getRawBits() << std::endl;
+// 	std::cout << c.getRawBits() << std::endl;
+
+// 	std::cout << a.getRawBits() << std::endl;
+// 	std::cout << b.getRawBits() << std::endl;
+// 	std::cout << c.getRawBits() << std::endl;
+// 	return 0;
+// }
