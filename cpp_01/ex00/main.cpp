3f060a694b262d5f83b5095fe3b93f698c38e9b3
mehdi boulhoujjat <60227776+boulhoujjatmehdi@users.noreply.github.com>
2023-09-13 14:59:15 +0100

    .

--- cpp_01/ex00/main.cpp
+++ cpp_01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: eboulhou <eboulhou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/10 19:21:24 by eboulhou          #+#    #+#             */
-/*   Updated: 2023/09/13 13:25:41 by eboulhou         ###   ########.fr       */
+/*   Updated: 2023/09/13 14:53:33 by eboulhou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,11 @@ int main()
     for (int i = 0;i < 10 ; i++)
     {
         string str = "mehdi-";
-        str += to_string(i);
+        str[5] = '0'+i;
         zombies[i] = Zombie(str);
         zombies[i].announce();
     }
-    
     randomChump("mehdi_stack");
-    //delete if you want to see the diff betweeen 
-    //allocating in heap and stack for the class zombie
     delete[] zombies;
-    // for(int i = 0 ; i < 10 ; i++)
-    // {
-    //     delete;
-    // }
+
 }
