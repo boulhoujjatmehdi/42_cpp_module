99efd6603a66ac2e13e096250060e3d1d089d054
mehdi boulhoujjat <60227776+boulhoujjatmehdi@users.noreply.github.com>
2023-09-14 14:44:41 +0100

    .

--- cpp_01/ex05/Harl.cpp
+++ cpp_01/ex05/Harl.cpp
@@ -1,50 +1,32 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Harl.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: eboulhou <eboulhou@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/17 16:43:53 by eboulhou          #+#    #+#             */
-/*   Updated: 2023/06/19 10:13:08 by eboulhou         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "Harl.hpp"
 
-void Harl::debug()
-{
-    cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << endl;
-}
-void Harl::info()
-{
-    cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"<< endl;
+void Harl::debug( void ){
+    cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!"<< endl;
 }
 
-void Harl::warning()
-{
-    cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << endl;
-}
-void Harl::error()
-{
-    cout << "This is unacceptable! I want to speak to the manager now." << endl;
+void Harl::info( void ){
+    cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << endl;
 }
 
-void Harl::complain( std::string level )
-{
-    std::map<string, void(Harl::*)()> functionMap;
-    functionMap["DEBUG"] = &Harl::debug;
-    functionMap["INFO"] = &Harl::info;
-    functionMap["WARNING"] = &Harl::warning;
-    functionMap["ERROR"] = &Harl::error;
-    
-    std::map<string, void (Harl::*)()>::iterator functionIt = functionMap.find(level);
-    if(functionIt != functionMap.end())
-    {
-        void (Harl::*functionPtr)() = functionIt->second;
-        (this->*functionPtr)();
-    }
+void Harl::warning( void ){
+    cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month."<< endl;
+}
 
+void Harl::error( void ){
+    cout << "This is unacceptable! I want to speak to the manager now."<< endl;
+}
 
-    (void)level;
+void Harl::complain(string level){
+
+    std::map<std::string, void(Harl::*)()> myMap;
+
+    myMap["DEBUG"] = &Harl::debug;
+    myMap["INFO"] = &Harl::info;
+    myMap["WARNING"] = &Harl::warning;
+    myMap["ERROR"] = &Harl::error;
+
+    Harl obj;
+    if(!(myMap.find(level) == myMap.end()))
+        (obj.*(myMap[level]))();
+
 }
--- cpp_01/ex05/Harl.hpp
+++ cpp_01/ex05/Harl.hpp
@@ -1,35 +1,26 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Harl.hpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: eboulhou <eboulhou@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/17 16:36:54 by eboulhou          #+#    #+#             */
-/*   Updated: 2023/09/13 12:40:04 by eboulhou         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-// #include <>
 #ifndef HARL_HPP
 #define HARL_HPP
 
+#include <string>
 #include <iostream>
+
 #include <map>
 
+
+using std::map;
+using std::string;
 using std::cout;
 using std::endl;
-using std::string;
 
 class Harl
 {
-    void debug(void);
-    void info(void);
-    void warning(void);
-    void error(void);
-
-public:
-    void complain( std::string level );
+    void debug( void );
+    void info( void );
+    void warning( void );
+    void error( void );
+    public:
+    void complain(string level);
 };
+
 
 #endif
